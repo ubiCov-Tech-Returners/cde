0801dbc3e1094c7a7894674ecaa38da73529c1ad
aczx131 <richard.renaud@city.co.uk>
2021-04-15 10:49:36 +0100

    Created GeoJson model.  Added furlough service call to a utility class 'GeoJsonGenerator'.
    Refactored GeoJsonGenerator to take a single parameter of type Map<String, String>
    
    ** TODO **
    Create test case to test GeoJsonGenerator

--- src/main/java/com/ubicov/app/util/geojson/Geometry.java
+++ src/main/java/com/ubicov/app/util/geojson/Geometry.java
@@ -1,3 +1,11 @@
+/**
+ * @Author - Richard Renaud, Carmen Lara.
+ * This Object will be used to generate GeoJson code.  This is not a persisted entity
+ * and it will be populated using OTHER DATA sets which are available to this system.
+ * <p>
+ * e.g.  The OTHER DATA has been stored on a DBMS which this system is granted access.
+ * This class is a subclass of the 'Feature' class.
+ */
 package com.ubicov.app.util.geojson;
 
 import lombok.*;
