0da13da3428ff905b37c0caf10c12c99762f812f
nherac <carmen@gamezlara.com>
2021-04-28 10:52:20 +0100

    Created Question Service and updated question controller

--- src/main/java/com/ubicov/app/controller/QuestionsController.java
+++ src/main/java/com/ubicov/app/controller/QuestionsController.java
@@ -1,15 +1,11 @@
 package com.ubicov.app.controller;
 
-import com.ubicov.app.domain.CovidData;
-import com.ubicov.app.domain.Furlough;
 import com.ubicov.app.service.*;
-import com.ubicov.app.util.geojson.Feature;
 import com.ubicov.app.util.geojson.MapInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,11 +22,13 @@ public class QuestionsController {
 /**
 * Will use CovidData dataset & FurloughData Dataset
 */
-    @GetMapping("/questions/{id}")
+    @GetMapping("/questions/{id}/")
     public List<MapInfo> question1(@PathVariable int id ){
         switch(id){
             case 1: return questionService.getQuestion1();
-            //case 2: return questionService.getQuestion2();
+            case 2: return questionService.getQuestion2();
+            case 3: return questionService.getQuestion3();
+            case 4: return questionService.getQuestion4();
         }
         return  null;
 
--- src/main/java/com/ubicov/app/service/QuestionService.java
+++ src/main/java/com/ubicov/app/service/QuestionService.java
@@ -2,12 +2,18 @@ package com.ubicov.app.service;
 
 import com.ubicov.app.domain.CovidData;
 import com.ubicov.app.domain.Furlough;
+import com.ubicov.app.domain.Vaccination;
 import com.ubicov.app.util.geojson.MapInfo;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+* Is vaccination uptake lesser in areas with lower income?
+Have covid cases fallen enough to safely reopen my business?
+Is the spread of Covid-19 in your area related to nights outs?
+* */
 
 @Service
 public class QuestionService {
@@ -25,7 +31,48 @@ public class QuestionService {
         this.furloughService = furloughService;
     }
 
+    /**
+     * This answer Question1
+     * Is vaccination uptake lesser in areas with lower income?
+     * @return
+     */
     public List<MapInfo> getQuestion1(){
+        /*
+
+        //Get list of covid
+        List<CovidData> covidList = new ArrayList<>();
+        //get list of vaccination
+        List<Vaccination> vaccinationList = new ArrayList<>();
+        // Combo list<<
+        ArrayList<MapInfo> combo = new ArrayList<>();
+
+        vaccinationList = vaccinationService.getAllVaccinations();
+        covidList = covidDataService.getAllCovid();
+
+        //Pending to investigate why doesn't work
+        for(Vaccination v: vaccinationList){
+            combo.add(
+                    vaccinationService.getVaccinationMapinfoByDistrict(v.getDistrict()));
+        }
+
+        for(CovidData c: covidList){
+            combo.add(covidDataService.getCovidCasesMapinfoByDistrict(c.getDistrict()));
+        }
+        System.out.println("Size is correct " + combo.size());
+        return combo;
+        */
+        return null;
+
+
+    }
+
+
+    /**
+     * This answer Question3
+     * How has Covid-19 affected your area?
+     * @return
+     */
+    public List<MapInfo> getQuestion3(){
         //Get list of covid
         List<CovidData> covidList = new ArrayList<>();
         //get list of Furlough
@@ -33,31 +80,30 @@ public class QuestionService {
         // Combo list<<
         ArrayList<MapInfo> combo = new ArrayList<>();
 
-
-        //Add Data to furlough
         furloughList = furloughService.getAllFurlough();
-        //Add Data to Covid
+
         covidList = covidDataService.getAllCovid();
-
-
-        // Use GeoJsonTransformer o generate json
-
-        //iterate furlough list - calling geoJsonTrans
         for(Furlough f: furloughList){
-            // ForEach record we get Mapinfo add to combo
             combo.add(
                     furloughService. getFurloughMapinfoByDistrict(f.getDistrict()));
         }
 
         for(CovidData c: covidList){
-            // ForEach record we get Mapinfo add to combo
             combo.add(covidDataService.getCovidCasesMapinfoByDistrict(c.getDistrict()));
         }
-
-        //iterate furlough list - calling geoJsonTrans
-        System.out.println("Size of combo is:  " + combo.size());
-
-
         return combo;
+    }
+
+    public List<MapInfo> getQuestion2() {
+        return null;
+    }
+
+    /**
+     * This answer Question4
+     * Is the spread of Covid-19 in your area related to nights outs?
+     * @return
+     */
+    public List<MapInfo> getQuestion4(){
+       return null;
     }
 }
