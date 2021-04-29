bb8539c0acba478d58effd17a7e9e57f6ff13553
nherac <carmen@gamezlara.com>
2021-04-29 08:30:26 +0100

    Updated Question Controller to return the http status 204 when there is not content

--- src/main/java/com/ubicov/app/controller/QuestionsController.java
+++ src/main/java/com/ubicov/app/controller/QuestionsController.java
@@ -2,6 +2,8 @@ package com.ubicov.app.controller;
 
 import com.ubicov.app.service.*;
 import com.ubicov.app.util.geojson.MapInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +25,16 @@ public class QuestionsController {
 * Will use CovidData dataset & FurloughData Dataset
 */
     @GetMapping("/questions/{id}/")
-    public List<MapInfo> question1(@PathVariable int id ){
+    public Object question1(@PathVariable int id ){
+        Object response = null;
         switch(id){
-            case 1: return questionService.getQuestion1();
-            case 2: return questionService.getQuestion2();
-            case 3: return questionService.getQuestion3();
-            case 4: return questionService.getQuestion4();
+            case 1: response = questionService.getQuestion1(); break;
+            case 2: response = questionService.getQuestion2(); break;
+            case 3: response =  questionService.getQuestion3(); break;
+            case 4: response = questionService.getQuestion4(); break;
         }
-        return  null;
+        return (response != null)? response:
+                                  (new ResponseEntity<Void>(HttpStatus.NO_CONTENT)) ;
 
     }
 }
